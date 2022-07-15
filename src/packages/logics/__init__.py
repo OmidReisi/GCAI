efd7e98c132e21675c65467d4a5f103ee9d9e203
OmidReisi <omid79.dg@gmail.com>
2022-07-15 16:29:55 +0430

    UPDATE: add movement for rest of the pieces

--- src/packages/logics/__init__.py
+++ src/packages/logics/__init__.py
@@ -13,23 +13,23 @@ def get_possible_moves(
 
         if piece == "P":
             return get_pawn_moves(board_state, selected_pos, turn_to_move)
-        elif piece == "N":
-            pass
-        elif piece == "B":
-            pass
-        elif piece == "R":
-            pass
-        elif piece == "Q":
-            pass
-        elif piece == "K":
-            pass
+        if piece == "N":
+            return get_knight_moves(board_state, selected_pos, turn_to_move)
+        if piece == "B":
+            return get_bishop_moves(board_state, selected_pos, turn_to_move)
+        if piece == "R":
+            return get_rook_moves(board_state, selected_pos, turn_to_move)
+        if piece == "Q":
+            return get_queen_moves(board_state, selected_pos, turn_to_move)
+        if piece == "K":
+            return get_king_moves(board_state, selected_pos, turn_to_move)
     else:
         return []
 
 
 def get_pawn_moves(
     board_state: list[list[str]], selected_pos: tuple[int, int], turn_to_move: str
-):
+) -> list[Move]:
     moves: list[Move] = []
     p_row, p_col = selected_pos
     if turn_to_move == "w":
@@ -119,3 +119,370 @@ def get_pawn_moves(
                     )
 
         return moves
+
+
+def get_knight_moves(
+    board_state: list[list[str]], selected_pos: tuple[int, int], turn_to_move: str
+) -> list[Move]:
+    moves: list[Move] = []
+    p_row, p_col = selected_pos
+    possitions_to_check = [
+        (p_row - 1, p_col + 2),
+        (p_row - 1, p_col - 2),
+        (p_row - 2, p_col + 1),
+        (p_row - 2, p_col - 1),
+        (p_row + 1, p_col + 2),
+        (p_row + 1, p_col - 2),
+        (p_row + 2, p_col - 1),
+        (p_row + 2, p_col + 1),
+    ]
+
+    for row, col in possitions_to_check:
+        if row in range(8) and col in range(8):
+            if board_state[row][col][0] != turn_to_move:
+                moves.append(Move(selected_pos, (row, col), board_state, turn_to_move))
+
+    return moves
+
+
+def get_bishop_moves(
+    board_state: list[list[str]], selected_pos: tuple[int, int], turn_to_move: str
+) -> list[Move]:
+    moves: list[Move] = []
+    directions = {
+        "upleft": True,
+        "upright": True,
+        "downleft": True,
+        "downright": True,
+    }
+    p_row, p_col = selected_pos
+
+    for i in range(1, 8):
+        if p_row + i in range(8):
+            if p_col + i in range(8):
+                possition_to_check = board_state[p_row + i][p_col + i]
+                if possition_to_check[0] == turn_to_move:
+                    directions["downright"] = False
+                elif directions["downright"]:
+
+                    if possition_to_check != "__":
+                        directions["downright"] = False
+                    moves.append(
+                        Move(
+                            selected_pos,
+                            (p_row + i, p_col + i),
+                            board_state,
+                            turn_to_move,
+                        )
+                    )
+            if p_col - i in range(8):
+                possition_to_check = board_state[p_row + i][p_col - i]
+                if possition_to_check[0] == turn_to_move:
+                    directions["downleft"] = False
+                elif directions["downleft"]:
+
+                    if possition_to_check != "__":
+                        directions["downleft"] = False
+                    moves.append(
+                        Move(
+                            selected_pos,
+                            (p_row + i, p_col - i),
+                            board_state,
+                            turn_to_move,
+                        )
+                    )
+
+        if p_row - i in range(8):
+            if p_col + i in range(8):
+                possition_to_check = board_state[p_row - i][p_col + i]
+                if possition_to_check[0] == turn_to_move:
+                    directions["upright"] = False
+                elif directions["upright"]:
+
+                    if possition_to_check != "__":
+                        directions["upright"] = False
+                    moves.append(
+                        Move(
+                            selected_pos,
+                            (p_row - i, p_col + i),
+                            board_state,
+                            turn_to_move,
+                        )
+                    )
+            if p_col - i in range(8):
+                possition_to_check = board_state[p_row - i][p_col - i]
+                if possition_to_check[0] == turn_to_move:
+                    directions["upleft"] = False
+                elif directions["upleft"]:
+
+                    if possition_to_check != "__":
+                        directions["upleft"] = False
+                    moves.append(
+                        Move(
+                            selected_pos,
+                            (p_row - i, p_col - i),
+                            board_state,
+                            turn_to_move,
+                        )
+                    )
+    return moves
+
+
+def get_rook_moves(
+    board_state: list[list[str]], selected_pos: tuple[int, int], turn_to_move: str
+) -> list[Move]:
+    moves: list[Move] = []
+
+    directions = {
+        "up": True,
+        "down": True,
+        "left": True,
+        "right": True,
+    }
+
+    p_row, p_col = selected_pos
+
+    for i in range(1, 8):
+        if p_row + i in range(8):
+            possition_to_check = board_state[p_row + i][p_col]
+            if possition_to_check[0] == turn_to_move:
+                directions["down"] = False
+            elif directions["down"]:
+
+                if possition_to_check != "__":
+                    directions["down"] = False
+                moves.append(
+                    Move(
+                        selected_pos,
+                        (p_row + i, p_col),
+                        board_state,
+                        turn_to_move,
+                    )
+                )
+        if p_row - i in range(8):
+            possition_to_check = board_state[p_row - i][p_col]
+            if possition_to_check[0] == turn_to_move:
+                directions["up"] = False
+            elif directions["up"]:
+
+                if possition_to_check != "__":
+                    directions["up"] = False
+                moves.append(
+                    Move(
+                        selected_pos,
+                        (p_row - i, p_col),
+                        board_state,
+                        turn_to_move,
+                    )
+                )
+        if p_col + i in range(8):
+            possition_to_check = board_state[p_row][p_col + i]
+            if possition_to_check[0] == turn_to_move:
+                directions["right"] = False
+            elif directions["right"]:
+
+                if possition_to_check != "__":
+                    directions["right"] = False
+                moves.append(
+                    Move(
+                        selected_pos,
+                        (p_row, p_col + i),
+                        board_state,
+                        turn_to_move,
+                    )
+                )
+        if p_col - i in range(8):
+            possition_to_check = board_state[p_row][p_col - i]
+            if possition_to_check[0] == turn_to_move:
+                directions["left"] = False
+            elif directions["left"]:
+
+                if possition_to_check != "__":
+                    directions["left"] = False
+                moves.append(
+                    Move(
+                        selected_pos,
+                        (p_row, p_col - i),
+                        board_state,
+                        turn_to_move,
+                    )
+                )
+    return moves
+
+
+def get_queen_moves(
+    board_state: list[list[str]], selected_pos: tuple[int, int], turn_to_move: str
+) -> list[Move]:
+    moves: list[Move] = []
+    directions = {
+        "up": True,
+        "down": True,
+        "left": True,
+        "right": True,
+        "upleft": True,
+        "upright": True,
+        "downleft": True,
+        "downright": True,
+    }
+    p_row, p_col = selected_pos
+
+    for i in range(1, 8):
+        if p_row + i in range(8):
+
+            possition_to_check = board_state[p_row + i][p_col]
+            if possition_to_check[0] == turn_to_move:
+                directions["down"] = False
+            elif directions["down"]:
+
+                if possition_to_check != "__":
+                    directions["down"] = False
+                moves.append(
+                    Move(
+                        selected_pos,
+                        (p_row + i, p_col),
+                        board_state,
+                        turn_to_move,
+                    )
+                )
+
+            if p_col + i in range(8):
+                possition_to_check = board_state[p_row + i][p_col + i]
+                if possition_to_check[0] == turn_to_move:
+                    directions["downright"] = False
+                elif directions["downright"]:
+
+                    if possition_to_check != "__":
+                        directions["downright"] = False
+                    moves.append(
+                        Move(
+                            selected_pos,
+                            (p_row + i, p_col + i),
+                            board_state,
+                            turn_to_move,
+                        )
+                    )
+            if p_col - i in range(8):
+                possition_to_check = board_state[p_row + i][p_col - i]
+                if possition_to_check[0] == turn_to_move:
+                    directions["downleft"] = False
+                elif directions["downleft"]:
+
+                    if possition_to_check != "__":
+                        directions["downleft"] = False
+                    moves.append(
+                        Move(
+                            selected_pos,
+                            (p_row + i, p_col - i),
+                            board_state,
+                            turn_to_move,
+                        )
+                    )
+
+        if p_row - i in range(8):
+
+            possition_to_check = board_state[p_row - i][p_col]
+            if possition_to_check[0] == turn_to_move:
+                directions["up"] = False
+            elif directions["up"]:
+
+                if possition_to_check != "__":
+                    directions["up"] = False
+                moves.append(
+                    Move(
+                        selected_pos,
+                        (p_row - i, p_col),
+                        board_state,
+                        turn_to_move,
+                    )
+                )
+
+            if p_col + i in range(8):
+                possition_to_check = board_state[p_row - i][p_col + i]
+                if possition_to_check[0] == turn_to_move:
+                    directions["upright"] = False
+                elif directions["upright"]:
+
+                    if possition_to_check != "__":
+                        directions["upright"] = False
+                    moves.append(
+                        Move(
+                            selected_pos,
+                            (p_row - i, p_col + i),
+                            board_state,
+                            turn_to_move,
+                        )
+                    )
+            if p_col - i in range(8):
+                possition_to_check = board_state[p_row - i][p_col - i]
+                if possition_to_check[0] == turn_to_move:
+                    directions["upleft"] = False
+                elif directions["upleft"]:
+
+                    if possition_to_check != "__":
+                        directions["upleft"] = False
+                    moves.append(
+                        Move(
+                            selected_pos,
+                            (p_row - i, p_col - i),
+                            board_state,
+                            turn_to_move,
+                        )
+                    )
+
+        if p_col + i in range(8):
+            possition_to_check = board_state[p_row][p_col + i]
+            if possition_to_check[0] == turn_to_move:
+                directions["right"] = False
+            elif directions["right"]:
+
+                if possition_to_check != "__":
+                    directions["right"] = False
+                moves.append(
+                    Move(
+                        selected_pos,
+                        (p_row, p_col + i),
+                        board_state,
+                        turn_to_move,
+                    )
+                )
+        if p_col - i in range(8):
+            possition_to_check = board_state[p_row][p_col - i]
+            if possition_to_check[0] == turn_to_move:
+                directions["left"] = False
+            elif directions["left"]:
+
+                if possition_to_check != "__":
+                    directions["left"] = False
+                moves.append(
+                    Move(
+                        selected_pos,
+                        (p_row, p_col - i),
+                        board_state,
+                        turn_to_move,
+                    )
+                )
+    return moves
+
+
+def get_king_moves(
+    board_state: list[list[str]], selected_pos: tuple[int, int], turn_to_move: str
+) -> list[Move]:
+    moves: list[Move] = []
+    p_row, p_col = selected_pos
+    possitions_to_check = [
+        (p_row - 1, p_col),
+        (p_row - 1, p_col - 1),
+        (p_row - 1, p_col + 1),
+        (p_row, p_col - 1),
+        (p_row, p_col + 1),
+        (p_row + 1, p_col),
+        (p_row + 1, p_col - 1),
+        (p_row + 1, p_col + 1),
+    ]
+
+    for row, col in possitions_to_check:
+        if row in range(8) and col in range(8):
+            if board_state[row][col][0] != turn_to_move:
+                moves.append(Move(selected_pos, (row, col), board_state, turn_to_move))
+
+    return moves
